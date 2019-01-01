be522f6e33b9d3f6870ff49e8288d99d3ff0c4ba
Eduard <dobryak.pc@gmail.com>
2019-01-01 16:59:08 +0300

    Это наш первый комит

---
+++ L1/script.js
@@ -0,0 +1,18 @@
+'use strict';
+
+console.log(4/0);
+console.log("1"/1);
+
+let arr = ["apple","juice"];
+
+console.log(arr[0]); 
+
+//alert("hello");
+
+// let answer = confirm("are u here?");
+
+// console.log(answer);
+
+let answer = prompt("are u here?");
+
+console.log(answer);
