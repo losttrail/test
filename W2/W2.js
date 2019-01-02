9d6dc509892d903a34cd47fbf59ae5f52ac3237c
Eduard <dobryak.pc@gmail.com>
2019-01-02 17:46:10 +0300

    hw 2

---
+++ W2/W2.js
@@ -0,0 +1,48 @@
+// Вопросы к этому заданию
+// Какое будет выведено значение: let x = 5; alert( x++ ); ? 5
+// let x = 5; alert( x++ );
+
+// Чему равно такое выражение: [ ] + false - null + true ? null, оказывается not a number NaN
+// let a = [ ] + false - null + true;
+// console.log(typeof(a));
+
+// Что выведет этот код: let y = 1; let x = y = 2; alert(x); ? 2
+// let y = 1; let x = y = 2; alert(x);
+
+// Чему равна сумма [ ] + 1 + 2? NaN, оказывается строка "12"
+// let a = [ ] + 1 + 2;
+// console.log(a);
+
+// Что выведет этот код: alert( "1"[0] )? 1
+// alert( "1"[0] );
+
+// Чему равно 2 && 1 && null && 0 && undefined ? false, оказывается null
+// a = 2 && 1 && null && 0 && undefined;
+// console.log(a);
+
+// Есть ли разница между выражениями? !!( a && b ) и (a && b)? нет, оказывается есть, !! заставляет придти к типу булева
+// let a = 0, b = 4;
+// c = !!( a && b );
+// d = (a && b);
+// console.log(c);
+// console.log(d);
+
+// Что выведет этот код: alert( null || 2 && 3 || 4 ); ? 3, первое непустое
+// alert( null || 2 && 3 || 4 );
+// a = null || 3
+// console.log(a);
+
+// a = [1, 2, 3]; b = [1, 2, 3]; Правда ли что a == b ? не правда
+// a = [1, 2, 3]; b = [1, 2, 3]; 
+// console.log(b);
+
+// Что выведет этот код: alert( +"Infinity" ); ? число Infinity
+// alert( +"Infinity" );
+
+// Верно ли сравнение: "ёжик" > "яблоко"? ё в юникоде больше
+// a = "ёжик" > "яблоко"; 
+// console.log(a);
+
+// Чему равно 0 || "" || 2 || undefined || true || falsе ? 2
+// a = 0 || "" || 2 || undefined || true || falsе;
+// console.log(a);
